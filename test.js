08059ad366697c98c563f7b7eb27232f31ecff30
Livingstone Sey <seylivingstones@gmail.com>
2022-10-13 22:51:58 +0000

    testing

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Test;
